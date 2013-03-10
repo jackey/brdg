eb041d27331af332a7b60dd432ecab7c43ecb2bf
jackey <jziwenchen@gmail.com>
2013-03-10 13:24:02 +0800

    添加必要模块

---
+++ sites/all/modules/brdg/brdg_twitter/brdg_twitter.settings.php
@@ -0,0 +1,16 @@
+<?php
+
+$settings = array(
+	'app_key' => '',
+	'app_secret' => '',
+	'access_token' => '',
+	'access_token_secret' => '',
+	'twitter_uid' => '568873580',
+	'search_tag' => array(
+		'#video'
+	),
+	'source_type' => 'Twitter Tweets',
+	'max_num' => 10,
+);
+
+return $settings;
