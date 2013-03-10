0a4a614689c6c99c478684163337565c78a67e35
jackey <jziwenchen@gmail.com>
2013-03-10 21:17:46 +0800

    添加instagram支持

---
+++ sites/all/modules/brdg/brdg_instagram/brdg_instagram.settings.php
@@ -0,0 +1,16 @@
+<?php
+
+$settings = array(
+	'app_key' => '61595534605540d081cb241cb002c7dc',
+	'app_secret' => '9005ae6fd69b439d819c3927d71f2ca8',
+	'access_token' => '',
+	'access_token_secret' => '',
+	'instagram_uid' => '323113616',
+	'search_tag' => array(
+		'loganneitzel'
+	),
+	'source_type' => 'Instagram',
+	'max_num' => 100,
+);
+
+return $settings;
--- sites/all/modules/brdg/brdg_twitter/brdg_twitter.settings.php
+++ sites/all/modules/brdg/brdg_twitter/brdg_twitter.settings.php
@@ -7,10 +7,10 @@ $settings = array(
 	'access_token_secret' => '',
 	'twitter_uid' => '568873580',
 	'search_tag' => array(
-		'#video'
+		'video'
 	),
 	'source_type' => 'Twitter Tweets',
-	'max_num' => 10,
+	'max_num' => 100,
 );
 
 return $settings;
