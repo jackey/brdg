c6cccedde5f0c82732d857122dd18581a0cf5736
jackey <jziwenchen@gmail.com>
2013-03-16 21:14:22 +0800

    .

---
+++ sites/all/modules/brdg/brdg_soundcloud/brdg_soundcloud.settings.php
@@ -0,0 +1,11 @@
+<?php
+
+$settings = array(
+	'search_tag' => array(
+		'hip'
+	),
+	'source_type' => 'Soundcloud',
+	'max_num' => 100,
+);
+
+return $settings;
---
+++ sites/all/modules/brdg/brdg_weibo/brdg_weibo.settings.php
@@ -0,0 +1,16 @@
+<?php
+
+$settings = array(
+	'app_key' => '',
+	'app_secret' => '',
+	'access_token' => '',
+	'access_token_secret' => '',
+	'weibo_uid' => '1790417857',
+	'search_tag' => array(
+		'video'
+	),
+	'source_type' => 'Weibo',
+	'max_num' => 100,
+);
+
+return $settings;
